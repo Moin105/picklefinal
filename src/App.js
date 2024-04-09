20b7a9bb7ad1c7220f689d6dbcfb21ebb5054d52
moin105 <moinlatif105@gmail.com>
2024-04-10 00:50:24 +0500

    werer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/Header/Header.jsx';
-import Banner from './Components/Banner/Banner.jsx';
-import Games from './Components/Games/Games.jsx';
-import SectionCollection from './Components/SectionCollection/SectionCollection.jsx';
-import Midsection from './Components/MidSection/Midsection.jsx';
-import Tokenomics from './Components/Tokenomics/Tokenomics.jsx';
-import LearnMore from './Components/LearnMore/LearnMore.jsx';
-import Footer from './Components/Footer/Footer.jsx';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header/Header.jsx";
+import Banner from "./Components/Banner/Banner.jsx";
+import Games from "./Components/Games/Games.jsx";
+import SectionCollection from "./Components/SectionCollection/SectionCollection.jsx";
+import Midsection from "./Components/MidSection/Midsection.jsx";
+import Tokenomics from "./Components/Tokenomics/Tokenomics.jsx";
+import LearnMore from "./Components/LearnMore/LearnMore.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
 function App() {
   return (
     <div className="App">
-  <Header/>
-  <Banner/>
-  <Games/>
-  {/*<Tokenomics/>*/}
-  <Midsection/>
-  <SectionCollection/>
-  <LearnMore/>
-<Footer/>
+      <Header />
+      <Banner />
+      <Games />
+      {/*<Tokenomics/>*/}
+      <Midsection />
+      <SectionCollection />
+      {/* <LearnMore/>*/}
+      <Footer />
     </div>
   );
 }
